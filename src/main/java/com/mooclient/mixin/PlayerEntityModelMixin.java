2cf72209d8dc8449b9fa0ea5fd3d40332ffebd1d
B4d-A1m <rafal658k2@gmail.com>
2026-09-04 00:08:51 +0200

    Release Moo Client v2.0.2: Globalna synchronizacja warstw skina 3D dla wszystkich emotek

--- src/main/java/com/mooclient/mixin/PlayerEntityModelMixin.java
+++ src/main/java/com/mooclient/mixin/PlayerEntityModelMixin.java
@@ -20,6 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Mixin do PlayerEntityModel aplikujący kąty kości ze zintegrowanego silnika EmoteEngine
  * w oparciu o interpolowane klatki animacji Blockbench.
+ * Gwarantuje perfekcyjne dopasowanie i synchronizację zewnętrznych warstw skina (3D layers)
+ * dla wszystkich emotek na raz, bez odrywania i podnoszenia warstw.
  */
 @Mixin(PlayerEntityModel.class)
 public abstract class PlayerEntityModelMixin extends BipedEntityModel<PlayerEntityRenderState> {
@@ -59,59 +61,91 @@ public abstract class PlayerEntityModelMixin extends BipedEntityModel<PlayerEnti
         float tickDelta = client.getRenderTickCounter().getTickDelta(true);
         emoteState.updateRenderTransforms(tickDelta);
 
-        // Aplikacja transformacji kości głowy (Head) - nadpisanie kątów animacją, by spojrzenie gracza nie kolidowało z rękami
+        // 1. Głowa (Head)
         BoneTransform headT = emoteState.getBoneTransform("head");
         if (headT != null) {
             this.head.pitch = headT.pitch;
             this.head.yaw = headT.yaw;
             this.head.roll = headT.roll;
+            if (headT.posX != 0.0f || headT.posY != 0.0f || headT.posZ != 0.0f) {
+                this.head.pivotX = headT.posX;
+                this.head.pivotY = -headT.posY;
+                this.head.pivotZ = -headT.posZ;
+            }
         }
 
-        // Aplikacja transformacji tułowia (Body)
+        // 2. Tułów (Body)
         BoneTransform bodyT = emoteState.getBoneTransform("body");
         if (bodyT != null) {
             this.body.pitch = bodyT.pitch;
             this.body.yaw = bodyT.yaw;
             this.body.roll = bodyT.roll;
+            if (bodyT.posX != 0.0f || bodyT.posY != 0.0f || bodyT.posZ != 0.0f) {
+                this.body.pivotX = bodyT.posX;
+                this.body.pivotY = -bodyT.posY;
+                this.body.pivotZ = -bodyT.posZ;
+            }
         }
 
-        // Aplikacja transformacji prawego ramienia (Right Arm)
+        // 3. Prawe ramię (Right Arm)
         BoneTransform rArmT = emoteState.getBoneTransform("right_arm");
         if (rArmT != null) {
             this.rightArm.pitch = rArmT.pitch;
             this.rightArm.yaw = rArmT.yaw;
             this.rightArm.roll = rArmT.roll;
+            if (rArmT.posX != 0.0f || rArmT.posY != 0.0f || rArmT.posZ != 0.0f) {
+                this.rightArm.pivotX = -5.0f + rArmT.posX;
+                this.rightArm.pivotY = 2.0f - rArmT.posY;
+                this.rightArm.pivotZ = -rArmT.posZ;
+            }
         }
 
-        // Aplikacja transformacji lewego ramienia (Left Arm)
+        // 4. Lewe ramię (Left Arm)
         BoneTransform lArmT = emoteState.getBoneTransform("left_arm");
         if (lArmT != null) {
             this.leftArm.pitch = lArmT.pitch;
             this.leftArm.yaw = lArmT.yaw;
             this.leftArm.roll = lArmT.roll;
+            if (lArmT.posX != 0.0f || lArmT.posY != 0.0f || lArmT.posZ != 0.0f) {
+                this.leftArm.pivotX = 5.0f + lArmT.posX;
+                this.leftArm.pivotY = 2.0f - lArmT.posY;
+                this.leftArm.pivotZ = -lArmT.posZ;
+            }
         }
 
-        // Aplikacja transformacji prawej nogi (Right Leg)
+        // 5. Prawa noga (Right Leg)
         BoneTransform rLegT = emoteState.getBoneTransform("right_leg");
         if (rLegT != null) {
             this.rightLeg.pitch = rLegT.pitch;
             this.rightLeg.yaw = rLegT.yaw;
             this.rightLeg.roll = rLegT.roll;
+            if (rLegT.posX != 0.0f || rLegT.posY != 0.0f || rLegT.posZ != 0.0f) {
+                this.rightLeg.pivotX = -1.9f + rLegT.posX;
+                this.rightLeg.pivotY = 12.0f - rLegT.posY;
+                this.rightLeg.pivotZ = -rLegT.posZ;
+            }
         }
 
-        // Aplikacja transformacji lewej nogi (Left Leg)
+        // 6. Lewa noga (Left Leg)
         BoneTransform lLegT = emoteState.getBoneTransform("left_leg");
         if (lLegT != null) {
             this.leftLeg.pitch = lLegT.pitch;
             this.leftLeg.yaw = lLegT.yaw;
             this.leftLeg.roll = lLegT.roll;
+            if (lLegT.posX != 0.0f || lLegT.posY != 0.0f || lLegT.posZ != 0.0f) {
+                this.leftLeg.pivotX = 1.9f + lLegT.posX;
+                this.leftLeg.pivotY = 12.0f - lLegT.posY;
+                this.leftLeg.pivotZ = -lLegT.posZ;
+            }
         }
 
-        // Synchronizacja warstw nakładek (sleeve, pants, jacket) z odpowiednimi kończynami
+        // 7. Pełna synchronizacja wszystkich zewnętrznych warstw skina (dla wszystkich emotek na raz)
+        // Zastępuje błędne resetTransform(), które podnosiło warstwy do góry
+        this.hat.copyTransform(this.head);
+        this.jacket.copyTransform(this.body);
         this.rightSleeve.copyTransform(this.rightArm);
         this.leftSleeve.copyTransform(this.leftArm);
         this.rightPants.copyTransform(this.rightLeg);
         this.leftPants.copyTransform(this.leftLeg);
-        this.jacket.copyTransform(this.body);
     }
 }
